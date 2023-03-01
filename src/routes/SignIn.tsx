0367f0e50ca8e7b3b7b3695555abadb3ae3dbaec
JellyKingdom <jyhan1993@gmail.com>
2023-03-01 20:33:53 +0900

    Feat: add style to Signin/Signup

--- src/routes/SignIn.tsx
+++ src/routes/SignIn.tsx
@@ -8,108 +8,134 @@ import { User } from "types";
 import { Wrapper, Button, Text, Input } from "components";
 
 const SignIn = () => {
-  const navigate = useNavigate();
-  const [_, setCookie] = useCookies(["Access-Token"]);
-  const [user, setUser] = useState<User>({
-    username: "",
-    password: "",
-  });
-
-  const { mutate } = useMutation(signin, {
-    onSuccess: (response) => {
-      if (response) {
-        setCookie("Access-Token", response?.headers.authorization.substr(7));
-        alert("로그인 성공!");
-        navigate("/");
-      }
-    },
-  });
-
-  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setUser({
-      ...user,
-      [name]: value,
+    const navigate = useNavigate();
+    const [_, setCookie] = useCookies(["Access-Token"]);
+    const [user, setUser] = useState<User>({
+        username: "",
+        password: "",
     });
-  };
 
-  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    const { mutate } = useMutation(signin, {
+        onSuccess: (response) => {
+            if (response) {
+                setCookie(
+                    "Access-Token",
+                    response?.headers.authorization.substr(7)
+                );
+                alert("로그인 성공!");
+                navigate("/");
+            }
+        },
+    });
 
-    if (user.username.trim() === "" || user.password.trim() === "") {
-      alert("모든 항목을 입력해주세요.");
-      return;
-    }
+    const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setUser({
+            ...user,
+            [name]: value,
+        });
+    };
 
-    mutate(user);
-    setUser({ username: "", password: "" });
-  };
+    const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-  return (
-    <>
-      <StContainer>
-        <StHeader>Sign In</StHeader>
-        <StForm onSubmit={onSubmitHandler}>
-          <Wrapper mg="10px 0">
-            <Text size="16">id</Text>
-          </Wrapper>
-          <Input
-            type="text"
-            onChange={onChangeHandler}
-            name="username"
-            value={user.username || ""}
-          />
-          <Wrapper mg="10px 0">
-            <Text size="16">password</Text>
-          </Wrapper>
-          <Input
-            type="password"
-            onChange={onChangeHandler}
-            name="password"
-            value={user.password || ""}
-          />
-          <Button mg="20px 0">로그인</Button>
-          <Wrapper>
-            회원이 아니신가요?
-            <Link to={"/signup"}>회원가입</Link>
-          </Wrapper>
-        </StForm>
-      </StContainer>
-    </>
-  );
+        if (user.username.trim() === "" || user.password.trim() === "") {
+            alert("모든 항목을 입력해주세요.");
+            return;
+        }
+
+        mutate(user);
+        setUser({ username: "", password: "" });
+    };
+
+    return (
+        <>
+            <StContainer>
+                <StHeader>Sign In</StHeader>
+                <StForm onSubmit={onSubmitHandler}>
+                    <StInputs>
+                        <Wrapper mg='10px 0'>
+                            <Text size='16'>id</Text>
+                        </Wrapper>
+                        <Input
+                            type='text'
+                            onChange={onChangeHandler}
+                            name='username'
+                            value={user.username || ""}
+                        />
+                        <Wrapper mg='10px 0'>
+                            <Text size='16'>password</Text>
+                        </Wrapper>
+                        <Input
+                            type='password'
+                            onChange={onChangeHandler}
+                            name='password'
+                            value={user.password || ""}
+                        />
+                    </StInputs>
+
+                    <Button mg='20px 0'>로그인</Button>
+                    <Wrapper>
+                        회원이 아니신가요?
+                        <Link to={"/signup"}><Text color='blue'>회원가입</Text></Link>
+                    </Wrapper>
+                </StForm>
+            </StContainer>
+        </>
+    );
 };
 
 export default SignIn;
 
 const StContainer = styled.div`
-  width: 67%;
-  max-height: 500px;
-  min-height: 700px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    width: 400px;
+    max-width: 100%;
+    background-color: rgb(255, 255, 255);
+    border-radius: 16px;
+    box-shadow: rgb(0 0 0 / 25%) 0px 14px 28px, rgb(0 0 0 / 22%) 0px 10px 10px;
+    position: relative;
+    overflow: hidden;
+    border: none;
 
-  background: #f5f5f5;
+    margin: 20px;
+    padding: 0;
 `;
 
 const StHeader = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
-  gap: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 10px;
+    gap: 10px;
+    color: white;
 
-  width: 100%;
-  height: 3vw;
-  background: #94c8b4;
-  border-radius: 16px 16px 0px 0px;
+    width: 100%;
+    height: 30px;
+    background: #94c8b4;
+    border-radius: 16px 16px 0px 0px;
 `;
 
 const StForm = styled.form`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+    width: 100%;
+    height: 100%;
+    min-height: 300px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 50px;
+
+    flex-direction: column;
+`;
+
+const StInputs = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+
+    flex-direction: column;
 `;
