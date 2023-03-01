b2efb0cc27bf3931aa20bd2210368f357a4a251a
JellyKingdom <jyhan1993@gmail.com>
2023-03-01 23:17:55 +0900

    Feat: add modal style

--- src/components/CardBoard/Card/Modal/index.tsx
+++ src/components/CardBoard/Card/Modal/index.tsx
@@ -9,161 +9,191 @@ import { motion } from "framer-motion";
 import { Card } from "types";
 import { likeCard, deleteCard } from "api";
 import { getYYYYMMDD } from "utils";
+import Button from "components/Elem/Button";
 
 interface Props {
-  clickedCard: Card;
-  setClickedCardId: (id: null) => void;
+    clickedCard: Card;
+    setClickedCardId: (id: null) => void;
 }
 
 const Modal = ({ clickedCard, setClickedCardId }: Props) => {
-  const overlay = {
-    hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
-    visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
-    exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
-  };
+    const overlay = {
+        hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+        visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+        exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+    };
 
-  const queryClient = useQueryClient();
-  const navigate = useNavigate();
-  const [cookies] = useCookies(["Access-Token"]);
-  const { mutate: likeCardMutate, data: likeCardData } = useMutation(
-    () => likeCard(clickedCard.id, cookies["Access-Token"]),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries("getCards");
-      },
-    }
-  );
-  const { mutate: deleteCardMutate, data: deleteCardData } = useMutation(
-    () => deleteCard(clickedCard.id, cookies["Access-Token"]),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries("getCards");
-      },
-    }
-  );
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+    const [cookies] = useCookies(["Access-Token"]);
+    const { mutate: likeCardMutate, data: likeCardData } = useMutation(
+        () => likeCard(clickedCard.id, cookies["Access-Token"]),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries("getCards");
+            },
+        }
+    );
+    const { mutate: deleteCardMutate, data: deleteCardData } = useMutation(
+        () => deleteCard(clickedCard.id, cookies["Access-Token"]),
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries("getCards");
+            },
+        }
+    );
 
-  useEffect(() => {
-    if (likeCardData) {
-      const { msg } = likeCardData.data;
-      alert(msg);
-    }
-  }, [likeCardData]);
+    useEffect(() => {
+        if (likeCardData) {
+            const { msg } = likeCardData.data;
+            alert(msg);
+        }
+    }, [likeCardData]);
 
-  useEffect(() => {
-    if (deleteCardData) {
-      const { msg } = deleteCardData.data;
-      alert(msg);
-    }
-  }, [deleteCardData]);
+    useEffect(() => {
+        if (deleteCardData) {
+            const { msg } = deleteCardData.data;
+            alert(msg);
+        }
+    }, [deleteCardData]);
 
-  const handleOnClickLikeBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
-    likeCardMutate();
-    setClickedCardId(null);
-  };
-  const handleOnClickEditBtn = () =>
-    navigate("/cards/edit", { state: { id: clickedCard.id } });
+    const handleOnClickLikeBtn = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+        likeCardMutate();
+        setClickedCardId(null);
+    };
+    const handleOnClickEditBtn = () =>
+        navigate("/cards/edit", { state: { id: clickedCard.id } });
 
-  const handleOnClickDelBtn = () => deleteCardMutate();
+    const handleOnClickDelBtn = () => deleteCardMutate();
 
-  return (
-    <StOverlay
-      variants={overlay}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
-      onClick={() => setClickedCardId(null)}
-    >
-      <StCardBox imgsrc={clickedCard.image} layoutId={clickedCard.id}>
-        {/* {clickedCard.nickname === "재훈님 최고" && ( */}
-        {true && (
-          <>
-            <button style={{ zIndex: "999" }} onClick={handleOnClickEditBtn}>
-              수정
-            </button>
-            <button style={{ zIndex: "999" }} onClick={handleOnClickDelBtn}>
-              삭제
-            </button>
-          </>
-        )}
-        <StContent>{clickedCard.content}</StContent>
-        <StCardFooter>
-          <div>{getYYYYMMDD(clickedCard.modifiedAt)}</div>
-          <StLikesWrapper>
-            <IconButton color="error" onClick={handleOnClickLikeBtn}>
-              <Favorite />
-            </IconButton>
-            <div>{clickedCard.likes}</div>
-          </StLikesWrapper>
-        </StCardFooter>
-      </StCardBox>
-    </StOverlay>
-  );
+    return (
+        <StOverlay
+            variants={overlay}
+            initial='hidden'
+            animate='visible'
+            exit='exit'
+            onClick={() => setClickedCardId(null)}
+        >
+            <StCardBox imgsrc={clickedCard.image} layoutId={clickedCard.id}>
+                {/* {clickedCard.nickname === "재훈님 최고" && ( */}
+                <StCardHeader>
+                    {true && (
+                        <>
+                            <button
+                                style={{ zIndex: "999" }}
+                                onClick={handleOnClickEditBtn}
+                            >
+                                수정
+                            </button>
+                            <button
+                                style={{ zIndex: "999" }}
+                                onClick={handleOnClickDelBtn}
+                            >
+                                삭제
+                            </button>
+                        </>
+                    )}
+                </StCardHeader>
+                <StBody>
+                    <StContent>{clickedCard.content}</StContent>
+                </StBody>
+                <StCardFooter>
+                    <div>{getYYYYMMDD(clickedCard.modifiedAt)}</div>
+                    <StLikesWrapper>
+                        <IconButton
+                            color='error'
+                            onClick={handleOnClickLikeBtn}
+                        >
+                            <Favorite />
+                        </IconButton>
+                        <div>{clickedCard.likes}</div>
+                    </StLikesWrapper>
+                </StCardFooter>
+            </StCardBox>
+        </StOverlay>
+    );
 };
 
 export default Modal;
 
 const StOverlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    z-index: 999;
+`;
+
+const StCardHeader = styled.div`
+    z-index: 99999;
+    display: flex;
+    justify-content: flex-end;
 `;
 
 const StCardBox = styled(motion.div)<{ imgsrc: string }>`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  width: 300px;
-  height: 300px;
-  border: 1px solid red;
-  border-radius: 10px;
-  padding: 10px;
-  cursor: pointer;
-  z-index: 9999;
+    text-align: center;
+    position: relative;
+    margin: auto 0;
+    width: 400px;
+    height: 400px;
+    /* border: 1px solid red; */
+    box-shadow: rgb(0 0 0 / 25%) 0px 14px 28px, rgb(0 0 0 / 22%) 0px 10px 10px;
+    /* border-radius: 10px; */
+    /* border-style: dotted; */
+    padding: 10px;
+    cursor: pointer;
+    z-index: 9999;
 
-  &::after {
-    width: 100%;
+    &::after {
+        width: 100%;
+        height: 100%;
+        content: "";
+        background-image: ${(props) => `url(${props.imgsrc})`};
+        background-size: cover;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+`;
+
+const StBody = styled.div`
     height: 100%;
-    content: "";
-    background-image: ${(props) => `url(${props.imgsrc})`};
-    background-size: cover;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
 `;
 
 const StContent = styled.div`
-  color: ${({ theme }) => theme.color.white};
-  background-color: ${({ theme }) => theme.color.black};
-  padding: 10px;
-  z-index: 999;
+    color: ${({ theme }) => theme.color.white};
+    background-color: ${({ theme }) => theme.color.black};
+    padding: 10px;
+    z-index: 999;
 `;
 
 const StCardFooter = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 30px;
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  padding: 10px 5px;
-  border: 1px solid red;
-  z-index: 999;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 30px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    padding: 20px 5px;
+    /* border: 1px solid red; */
+    z-index: 999;
+    background-color: white;
 `;
 
 const StLikesWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
